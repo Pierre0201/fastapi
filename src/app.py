8cfc7273329a7cea83d4c8d04984bef1cdad4063
Pierre0201 <108532397+Pierre0201@users.noreply.github.com>
2022-07-29 01:08:23 +0200

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1,22 +1,29 @@
-# 1. Library imports
 import uvicorn
 from fastapi import FastAPI
+import pandas as pd
+from joblib import load
+from io import BytesIO
+import requests
+
+mLink = 'https://github.com/Pierre0201/fastapi/blob/main/src/ressource/clf.joblib?raw=true'
+mfile = BytesIO(requests.get(mLink).content)
+clf = load(mfile)
 
 # 2. Create the app object
 app = FastAPI()
+
+path = 'https://raw.githubusercontent.com/Pierre0201/fastapi/main/src/ressource/'
+test_df = pd.read_csv(path+'submission_kernel02.csv')
+feats = [f for f in test_df.columns if f not in ['TARGET','SK_ID_CURR','SK_ID_BUREAU','SK_ID_PREV','index']]
 
 # 3. Index route, opens automatically on http://127.0.0.1:8000
 @app.get('/')
 def index():
     return {'message': 'Hello, stranger'}
 
-# 4. Route with a single parameter, returns the parameter within a message
-#    Located at: http://127.0.0.1:8000/AnyNameHere
-@app.get('/{name}')
-def get_name(name: str):
-    return {'message': f'Hello, {name}'}
-
-# 5. Run the API with uvicorn
-#    Will run on http://127.0.0.1:8000
-if __name__ == '__main__':
-    uvicorn.run(app, host='127.0.0.1', port=8000)
+@app.post('/predict')
+def predict():
+    y = clf.predict_proba(test_df[feats].loc[test_df['SK_ID_CURR']==100141], num_iteration=clf.best_iteration_)[:,1]
+    return {
+        'prediction': y[0]
+        }
